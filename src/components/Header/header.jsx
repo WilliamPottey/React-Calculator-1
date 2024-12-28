04e631e013cd301212a6cd9a147dc1d0075ad80d
WilliamPottey <wottey9007@gmail.com>
2024-12-28 15:54:18 -0500

    add navbar + update navbar

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./header.css";
+import linkedIn from "./images/linkedIn.png";
+import GitHub from "./images/github-logo.png";
+import "bootstrap/dist/js/bootstrap.js";
+
+function test() {
+  console.log("test function");
+}
+
+const header = ({ page }) => {
+  return (
+    <header>
+      <nav className="navbar fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand">
+            William Pottey | B.S. Computational Mathematics
+          </a>
+          <a href="https://williampottey.github.io/William-Pottey-Portfolio/">
+            <button className="btn btn-secondary home">Home</button>
+          </a>
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="project-dropdown"
+              data-bs-toggle="dropdown"
+            >
+              Projects
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="project-dropdown">
+              <li>
+                <a
+                  href="#"
+                  onClick={() => window.location.reload()}
+                  className="dropdown-item"
+                >
+                  Calculator
+                </a>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="nav-item"
+            id="socialMediaButtons"
+            href="https://github.com/WilliamPottey"
+            target="_blank"
+          >
+            <img className="socials" src={GitHub} width={35} title="Github" />
+          </a>
+          <a
+            className="nav-item"
+            // id="socialMediaButtons"
+            href="https://www.linkedin.com/in/williampottey/"
+            target="_blank"
+          >
+            <img
+              className="socials"
+              src={linkedIn}
+              width={35}
+              title="LinkedIn"
+            />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Header from "./components/Header/header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
     <span id="calc_text">Calculator</span>
     <App />
   </StrictMode>
